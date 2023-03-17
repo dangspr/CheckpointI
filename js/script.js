8f8b63d425f270a4d4a6d7a495e81fbc9d6b3532
DaniloG <dan_gaaspar@hotmail.com>
2023-03-16 21:52:08 -0300

    Alter JS and CSS General

--- js/script.js
+++ js/script.js
@@ -1,22 +1,83 @@
-let contantRef = document.querySelector('#contantRef');
+const inputNickname = document.querySelector('#nick');
+const inputChampion = document.querySelector('#champion');
+const inputLane = document.querySelector('#lane');
+let contentRef = document.querySelector('#content');
+const registerBtnRef = document.querySelector("#registerBtn");
+const charFormRef = document.querySelector("#charForm");
 
-let champions = [
-    {
-        titulo: 'Garen',
-        descricao: `Um guerreiro nobre e orgulhoso, Garen faz parte da Vanguarda Destemida. 
-        Popular entre seus companheiros e respeitado o suficiente por seus inimigos, sua reputação é nada mais do que o esperado de um herdeiro da prestigiosa
-        família Stemmaguarda, encarregada de defender Demacia e seus ideais. Vestido com uma armadura resistente à magia e empunhando uma poderosa espada,
-        Garen está sempre pronto para confrontar magos e feiticeiros no campo de batalha, em um verdadeiro furacão de aço virtuoso.`,
-        url: './img/garen'
-    },
 
-]
-for (let champions of champions) {
-    contantRef.innerHTML += `
-      <div class="item">
-        <img src="${champions.url}">
-        <h2>${champions.titulo}</h2>
-        <p>${champions.descricao}</p>
-      </div>
-    `
+var formErrors = {
+    nick: true  ,
+    champion: true,
+    lane: true
+};
+
+function checkFormValidity() {
+    const formErrorsArray = Object.values(formErrors);
+    const formValidity = formErrorsArray.every(item => item === false);
+
+    registerBtnRef.disabled = !formValidity;
 }
+
+function validateInput(inputRef) {
+    const inputValid = inputRef.checkValidity();
+    const elementFatherRef = inputRef.parentElement;
+
+    if (inputValid) {
+        elementFatherRef.classList.remove('error');
+    } else {
+        elementFatherRef.classList.add('error');
+    }
+
+    formErrors[inputRef.id] = !inputValid;
+
+    checkFormValidity();
+}
+
+function register(event) {
+
+    event.preventDefault();
+
+    let characters = [];
+
+    let character = {
+        nick: "",
+        champion: "",
+        lane: ""
+    }
+
+    character.nick = document.querySelector("#nick").value;
+    character.champion = document.querySelector("#champion").value;
+    character.lane = document.querySelector("#lane").value;
+
+    characters.push(character);
+
+    console.log(characters);
+
+    for (const char of characters) {
+        contentRef.innerHTML += `
+                <div class="card">
+                <div class="card-image">
+                <img src="${char.imgURL}" alt="">
+            </div>
+                    <div class="card-content">
+                        <h3>${char.nick}</h3>
+                        <h2>${char.champion}</h2>
+                        <p>${char.lane}</p>
+                    </div>        
+                </div>`
+    }
+
+    registerBtnRef.disabled = true;
+    charFormRef.reset();
+
+    formErrors.nick = true;
+    formErrors.champion = true;
+    formErrors.lane = true;
+
+}
+
+inputNickname.addEventListener('keyup', () => validateInput(inputNickname));
+inputChampion.addEventListener('keyup', () => validateInput(inputChampion));
+inputLane.addEventListener('keyup', () => validateInput(inputLane));
+registerBtnRef.addEventListener('click', (event) => register(event));
